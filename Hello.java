9323198908b56eeddc14a8d52bec569ddc96754a
Omar <o.moreta@gmail.com>
2023-10-22 23:03:54 -0400

    jdk installed and first program compiled and executed

---
+++ Hello.java
@@ -0,0 +1,15 @@
+class Hello {
+    public static void main ( String[] args ) {
+        // testing out compilation and execution
+        System.out.println("Hello World!");
+
+        System.out.println("------------------------");
+        
+        // testing variables
+        String message = "Initial value";
+        System.out.println( message );
+        message = "Modified value";
+        System.out.println( message );
+    }
+}
+
